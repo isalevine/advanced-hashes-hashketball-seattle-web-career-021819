8fce7704ae36c7c61dab23371e55882da629d8b9
Isa Levine <isaacdlevine@gmail.com>
2019-01-23 00:53:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -125,8 +125,7 @@ def num_points_scored(player_name_input)
     players_hashes.each do |the_player_name, player_data_hash|
       if the_player_name = player_name_input
         binding.pry
-        player_key = player_name_input
-        points = team_data[:players][player_key][:points]
+        points = team_data[:players][player_name_input][:points]
       end
     end
   end
